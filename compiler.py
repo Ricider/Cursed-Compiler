8e8479e3a964a7aab22207cbf50b3edd9726e55f
Sarp <54453420+Ricider@users.noreply.github.com>
2020-03-28 21:51:03 +0300

    Update compiler.py

--- compiler.py
+++ compiler.py
@@ -66,7 +66,7 @@ asmBlocks=[block("add",None,
               "movq 16(%rsp), %rbx\n"+
               "movq $1, %r9\n"+
               "cmp %rbx, %r9\n"+
-              "jge isFalse\n"+
+              "jg isFalse\n"+
               "movq $0, 24(%rsp)\n"+
               "ret\n"+
               "isFalse:\n"+
